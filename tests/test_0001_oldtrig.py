b2b6c28bdff6766b1427564b30177e8d0e563e6d
Jim Pivarski <jpivarski@gmail.com>
2024-05-10 17:06:37 -0500

    try putting the tests in JupyterLite, too

---
+++ tests/test_0001_oldtrig.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import math
+
+import pytest
+
+from my_cool_package import oldtrig as old
+
+
+@pytest.mark.parametrize(
+    ("arg", "expected"),
+    [
+        (-10, -0.17364817766693033),
+        (0, 0),
+        (10, 0.17364817766693033),
+        (30, 0.5),
+        (45, 1 / math.sqrt(2)),
+        (60, math.sqrt(3) / 2),
+        (80, 0.984807753012208),
+        (90, 1),
+        (100, 0.984807753012208),
+        (180, 0),
+        (270, -1),
+        (360, 0),
+        (370, 0.17364817766693033),
+    ],
+)
+def test_values(arg, expected):
+    assert old.sine(arg) == pytest.approx(expected)
--- tests/test_package.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import importlib.metadata
-
-import my_cool_package as m
-
-
-def test_version():
-    assert importlib.metadata.version("my_cool_package") == m.__version__
